f1111c75e8c25cc7b9abf74b28a7e882aea9ef32
Kou <kimura@kurashicom.jp>
2018-11-04 22:16:45 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    date: '11月4日 [日]',
+    img: 'https://assets.kurashicom.com/blog/uploads/2018/11/01175800/DSCF2160.jpg',
+    text: '多少チグハグなくらいが、私らしいのかも'
+  }
+})
